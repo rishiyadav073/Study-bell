5a5dfc8ea6409370bed0b1105badcc37beda79ef
rishiyadav073 <royalrishi0802@gmail.com>
2026-05-09 21:36:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <defs>
+    <linearGradient id="bg" x1="88" x2="424" y1="62" y2="450" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#2d6cdf"/>
+      <stop offset="0.58" stop-color="#7d5cff"/>
+      <stop offset="1" stop-color="#12a88a"/>
+    </linearGradient>
+    <linearGradient id="page" x1="154" x2="358" y1="128" y2="382" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#ffffff"/>
+      <stop offset="1" stop-color="#eaf2ff"/>
+    </linearGradient>
+  </defs>
+  <rect width="512" height="512" rx="108" fill="url(#bg)"/>
+  <path fill="#ffffff" fill-opacity="0.22" d="M84 133c38-61 104-90 182-87 78 4 136 39 169 106 28 58 19 122-21 183-46 69-113 103-199 99-78-3-134-43-167-111-31-64-19-129 36-190Z"/>
+  <path fill="url(#page)" d="M158 128h151c29 0 52 23 52 52v184c0 11-9 20-20 20H158c-11 0-20-9-20-20V148c0-11 9-20 20-20Z"/>
+  <path fill="#d8e6ff" d="M361 183v181c0 11-9 20-20 20H171V183c0-11 9-20 20-20h135c19 0 35 10 35 20Z"/>
+  <path fill="#172033" d="M195 210h111c8 0 14 6 14 14s-6 14-14 14H195c-8 0-14-6-14-14s6-14 14-14Zm0 56h94c8 0 14 6 14 14s-6 14-14 14h-94c-8 0-14-6-14-14s6-14 14-14Z"/>
+  <path fill="#f4b740" d="M326 100 430 144 326 188 222 144l104-44Z"/>
+  <path fill="#d99a20" d="M260 161v49c0 18 30 33 66 33s66-15 66-33v-49l-66 27-66-27Z"/>
+  <path fill="#172033" d="M418 151h20v86h-20z"/>
+  <circle cx="428" cy="252" r="18" fill="#f4b740"/>
+  <path fill="#12a88a" d="M210 327c7-7 18-7 25 0l18 18 54-60c6-7 17-8 24-2 7 7 8 17 2 24l-67 75c-3 4-8 6-13 6s-10-2-13-5l-30-31c-7-7-7-18 0-25Z"/>
+</svg>
